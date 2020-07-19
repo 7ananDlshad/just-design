b6f244b06607f537241c1f20405cd1523c1b87a9
Hanan <ensw03217@student.su.edu.krd>
2020-07-19 18:37:46 +0300

    find bus

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./about.css";
+import { Button } from "react-bootstrap";
 
 export default function About() {
   return (
     <>
-      <div className="bg-image"></div>
+      <div className="bg-image">
+        <div style={{ padding: "8% 14%" }}>
+          <h1>TAKE A BUS NOW </h1>
+          <p style={{ paddingLeft: "13%" }}>or</p>
+          <h4 className="px-3">Miss your chance forever</h4>
+          <Button variant="submit" type="submit" className="px-5 my-2">
+            Find Bus
+          </Button>
+        </div>
+      </div>
     </>
   );
 }
