675cc18fe3bac7bce07f93fd14e75f94f1b34b89
Hanan <ensw03217@student.su.edu.krd>
2020-07-20 09:22:27 +0300

    What do we give you?(section)

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import "./circle.css";
-import { Button } from "react-bootstrap";
+import { Button, Row, Col } from "react-bootstrap";
 export default function About() {
   return (
     <>
@@ -64,7 +64,7 @@ export default function About() {
 
               <div className="progress-value">90%</div>
             </div>
-            <h5 className='my-4'>students</h5>
+            <h5 className="my-4">students</h5>
           </div>
           <div className="col-md-3 col-sm-6">
             <div className="progress green">
@@ -76,7 +76,7 @@ export default function About() {
               </span>
               <div className="progress-value">85%</div>
             </div>
-            <h5 className='my-4'>old peoples</h5>
+            <h5 className="my-4">old peoples</h5>
           </div>
           <div className="col-md-3 col-sm-6">
             <div className="progress yellow">
@@ -88,9 +88,72 @@ export default function About() {
               </span>
               <div className="progress-value">75%</div>
             </div>
-            <h5 className='my-4'>teenagers</h5>
+            <h5 className="my-4">teenagers</h5>
           </div>
         </div>
+      </div>
+
+      <div className="text-center" style={{ marginTop: "7%" }}>
+        <h1>What do we give you?</h1>
+        <hr style={{ width: "30%" }} />
+        <Row>
+          <Col lg={5} style={{ marginTop: "7%" }}>
+            <Row>
+              <Col className="d-flex justify-content-end align-item-center">
+                <img
+                  src={require("../images/time.png")}
+                  alt="my pic"
+                  style={{ width: "33%", height: "67%" }}
+                />
+              </Col>
+              <Col>
+                <h4>Check the time </h4>
+                <small>
+                  You can go to the route time of the busses and check the best
+                  time for you.
+                </small>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="d-flex justify-content-end align-item-center py-5">
+                <img
+                  src={require("../images/route.png")}
+                  alt="my pic"
+                  style={{ width: "35%", height: "78%" }}
+                />
+              </Col>
+              <Col className="py-5">
+                <h4>Check your route </h4>
+                <small>
+                  Choose the best route for you and for your travel.
+                </small>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="d-flex justify-content-end align-item-center">
+                <img
+                  src={require("../images/destination.png")}
+                  alt="my pic"
+                  style={{ width: "35%", height: "78%" }}
+                />
+              </Col>
+              <Col>
+                <h4>Get to your destination </h4>
+                <small>
+                  Go in the bus and get to your place and enjoy your time.
+                </small>
+              </Col>
+            </Row>
+          </Col>
+
+          <Col lg={7}>
+            <img
+              src={require("../images/take-bus.png")}
+              alt="my pic"
+              style={{ width: "100%", height: "100%" }}
+            />
+          </Col>
+        </Row>
       </div>
     </>
   );
